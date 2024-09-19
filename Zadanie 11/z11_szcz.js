11281241902ff52c6e83058407942398ff17e7da
flamingphoenixxx@gmail.com <MiloszSzczechowski>
2024-09-19 10:48:04 +0200

    Z11

---
+++ Zadanie 11/z11_szcz.js
@@ -0,0 +1,6 @@
+let d1_szcz = 30
+let d2_szcz = 28
+
+let seconds = (d1_szcz - d2_szcz) * 24 * 60 * 60
+
+document.write(`${seconds}`.fontcolor("green").fontsize(40))
--- window.js
+++ window.js
@@ -1,5 +1,5 @@
-function WinOpen_szczechowski(file, window_name, height=380, width=160, top=200, left=200) {
-	task_window = window.open(
+function WinOpen_szczechowski(file, window_name, height=1080, width=1920, top=0, left=0) {
+	window.open(
 		file,
 		window_name,
 		`toolbar=no,directories=no,menubar=no, height=${height}, width=${width}, top=${top}, left=${left}`
